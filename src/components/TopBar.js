41ff5a5dfac0a7ba5992a8bc84669d13c524be8b
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-04-16 18:46:45 +0200

    finished topbar component

---
+++ src/components/TopBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./style/TopBar.css";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function App() {
+  return (
+    <nav className="wrapper">
+      <ul className="flex-container">
+        <li>
+          <h1>NIGHT</h1>
+        </li>
+        <li>
+          <button>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          <input placeholder="Buscar Eventos"></input>
+        </li>
+        <li>
+          <button>
+            <span hidden className="menu">
+              Menú
+            </span>
+            <FontAwesomeIcon className="menu-icon" icon={faBars} />
+          </button>
+        </li>
+        <li>
+          <button>Iniciar Sesión</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default TopBar;
