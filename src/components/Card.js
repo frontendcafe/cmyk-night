0c7ede816dcf356562cd9d0b4aa9d7acb7e931ab
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-04-21 15:04:37 -0400

    fix details with the name of the classes and the images of the cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,11 @@ import "./style/Card.css"
 function Card({ imageSrc, name, rate }) {
     return (
         <div>
-            <div className="container_picture">
-                <img className="container_image" src={imageSrc} alt={name} />
+            <div className="container__picture">
+                <img className="container__image" src={imageSrc} alt={name} />
             </div>
-            <p className="container_text">{name}</p>
-            <ul className="container_stars">
+            <p className="container__text">{name}</p>
+            <ul className="container__stars">
                 <li>
                     <Star />
                 </li>
