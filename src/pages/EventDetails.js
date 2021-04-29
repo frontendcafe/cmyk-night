9817233861277b11e46ea2cec10fde8a04856ea8
Shorsh <jorge@cliengo.com>
2021-04-29 18:57:34 -0300

    merge

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -10,7 +10,7 @@ import "../components/style/EventDetails.css"
 
 export default function EventDetail() {
   const { eventId } = useParams()
-  
+
   const mock = [{day: 1, enabled: true, hours: [{from: '2:00'}, {from: '3:00'}]},
               {day: 2, enabled: true, hours: [{from: '3:00'}, {from: '4:00'}]},
               {day: 3, enabled: true, hours: [{from: '3:00'}, {from: '5:00'}]}]
@@ -20,7 +20,7 @@ export default function EventDetail() {
     {screenWidth >= 720 ?
       <DescriptionCard title="SEREMOS PRIMAVERA" location="Eruca Sativa" description="Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum" banner_image={imgp} />
       :
-      <>
+      <div className="ed_container">
         <div className="maincard">
           <MainCard date={"1-2-3 Marzo"} title={"seremos primavera"} performer={"Eruca Sativa"} banner_image={"https://picsum.photos/200/120"}></MainCard>
         </div>
@@ -28,12 +28,12 @@ export default function EventDetail() {
           <StarList />
           <p className="description__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque purus ipsum, pellentesque quis massa a, imperdiet fringilla tortor. Nunc maximus ligula id est blandit, at blandit arcu cursus. In at metus sapien. Sed mauris lorem</p>
         </div>
-      </>
+      </div>
     }
 
     <div className= "dates_container">
       <DatesContainer  dates={mock}/>
     </div>
-    <Link to={`/checkout/${eventId}`} className="btn">Reservar</Link>
+    <Link to={`/checkout/${eventId}`} className={'btn'}>Reservar</Link>
   </div>
 }
