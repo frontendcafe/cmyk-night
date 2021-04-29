b925a13887f2efeb05efa832e3a4d7eed7938c0b
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-04-29 15:49:32 -0400

    Add Description Card, Create Starlist component

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -3,31 +3,28 @@ import { Link, useParams } from 'react-router-dom';
 import MainCard from "../components/MainCard";
 import DatesContainer from '../components/DatesContainer';
 import imgp from "../components/img/imgPrueba.jpg"
+import DescriptionCard from "../components/DescriptionCard";
+import StarList from "../components/StarList"
 import "../components/style/EventDetails.css"
-import { ReactComponent as Star } from "../components/icons/star.svg";
 
 export default function EventDetail() {
   const { eventId } = useParams()
   const mock = [{ day: 1, enabled: true, hours: [{ from: '2:00' }, { from: '3:00' }] }]
+
+  const screenWidth = window.screen.width;
   return <div>
-    <MainCard date={"24 enero"} title={"seremos primavera"} performer={"Eruca Sativa"} banner_image={imgp} />
-    <div className="description">
-      <ul className="container__stars">
-        <li>
-          <Star />
-        </li>
-        <li>
-          <Star />
-        </li>
-        <li>
-          <Star />
-        </li>
-        <li>
-          <Star />
-        </li>
-      </ul>
-      <p className="description__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque purus ipsum, pellentesque quis massa a, imperdiet fringilla tortor. Nunc maximus ligula id est blandit, at blandit arcu cursus. In at metus sapien. Sed mauris lorem</p>
-    </div>
+    {screenWidth >= 720 ?
+      <DescriptionCard title="SEREMOS PRIMAVERA" location="Eruca Sativa" description="Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum" banner_image={imgp} />
+      :
+      <>
+        <MainCard date={"24 enero"} title={"seremos primavera"} performer={"Eruca Sativa"} banner_image={imgp} />
+        <div className="description">
+          <StarList />
+          <p className="description__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque purus ipsum, pellentesque quis massa a, imperdiet fringilla tortor. Nunc maximus ligula id est blandit, at blandit arcu cursus. In at metus sapien. Sed mauris lorem</p>
+        </div>
+      </>
+    }
+
     <DatesContainer dates={mock} />
     <Link to={`/checkout/${eventId}`}>Reservar</Link>
   </div>
