aeab04d5b68c522546ab672f30789a547627c724
Florencia Licarzi <37675631+florencialicarzi@users.noreply.github.com>
2021-04-30 12:38:30 -0300

    fix: event_layout

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -11,29 +11,32 @@ import "../components/style/EventDetails.css"
 export default function EventDetail() {
   const { eventId } = useParams()
 
-  const mock = [{day: 1, enabled: true, hours: [{from: '2:00'}, {from: '3:00'}]},
-              {day: 2, enabled: true, hours: [{from: '3:00'}, {from: '4:00'}]},
-              {day: 3, enabled: true, hours: [{from: '3:00'}, {from: '5:00'}]}]
+  const mock = [{day: 24, enabled: true, hours: [{from: '2:00'}, {from: '3:00'}]},
+              {day: 25, enabled: true, hours: [{from: '3:00'}, {from: '4:00'}]},
+              {day: 26, enabled: true, hours: [{from: '3:00'}, {from: '5:00'}]}]
 
   const screenWidth = window.screen.width;
-  return <div>
+  return <div className="ed_container">
     {screenWidth >= 720 ?
-      <DescriptionCard title="SEREMOS PRIMAVERA" location="Eruca Sativa" description="Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum" banner_image={imgp} />
+      <div className="maincard"> 
+        <DescriptionCard title="SEREMOS PRIMAVERA" location="Eruca Sativa" description="Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum" banner_image={imgp} />
+      </div>
       :
-      <div className="ed_container">
+     
         <div className="maincard">
           <MainCard date={"1-2-3 Marzo"} title={"seremos primavera"} performer={"Eruca Sativa"} banner_image={"https://picsum.photos/200/120"}></MainCard>
+          <div className="description">
+            <StarList />
+            <p className="description__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque purus ipsum, pellentesque quis massa a, imperdiet fringilla tortor. Nunc maximus ligula id est blandit, at blandit arcu cursus. In at metus sapien. Sed mauris lorem</p>
+          </div>
         </div>
-        <div className="description">
-          <StarList />
-          <p className="description__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque purus ipsum, pellentesque quis massa a, imperdiet fringilla tortor. Nunc maximus ligula id est blandit, at blandit arcu cursus. In at metus sapien. Sed mauris lorem</p>
-        </div>
-      </div>
+    
     }
 
     <div className= "dates_container">
       <DatesContainer  dates={mock}/>
     </div>
     <Link to={`/checkout/${eventId}`} className={'btn'}>Reservar</Link>
+
   </div>
 }
