bd8adbb28d649d14731add2ded17128bba623fd9
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-05-02 18:48:58 -0400

    Create the LineSeparator Component

---
+++ src/components/LineSeparator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style/LineSeparator.css";
+
+const LineSeparator = () => (
+    <div className="lineSeparator">
+        <hr className="lineSeparator__line" />
+            o
+        <hr className="lineSeparator__line" />
+    </div>
+);
+
+export default LineSeparator;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import LineSeparator from '../components/LineSeparator';
 import SocialLogin from "../components/SocialLogin";
 import "./style/Login.css";
 
@@ -22,11 +23,7 @@ const Login = () => {
                     <Link className="LoginCard__forgotPassword" to="/">¿Olvidate tu contraseña?</Link>
                     <Button className="LoginCard__button"  onClick={handleSubmit}>Iniciar sesion</Button>
                 </form>
-                <div className="LoginCard__lineSeparator">
-                    <hr className="LoginCard__line" />
-                    o
-                    <hr className="LoginCard__line" />
-                </div>
+                <LineSeparator />
                 <SocialLogin />
                 <div className="LoginCard__ContainerNoAcount">
                     <Link className="LoginCard__NoAcount" to="/signup">¿Aún no tienes cuenta? Registrate</Link>
