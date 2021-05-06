411861fe0327496c760157c739b12ee9ce049297
Florencia Licarzi <37675631+florencialicarzi@users.noreply.github.com>
2021-05-06 20:42:22 -0300

    add components on checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import FechaHoraLugar from '../components/FechaHoraLugar';
+import Avatar from '../components/Avatar';
+import "./style/Checkout.css"
+import Input from "../components/Input";
+
 
 const Checkout = () => {
-  const { eventId } = useParams()
-  return <>
-    <div>
-    Checkout {eventId}
+
+  return <div className="check_container">
+
+    <div className="check_header">
+      <Avatar src="https://picsum.photos/200" name="SEREMOS PRIMAVERA"></Avatar>
+      <FechaHoraLugar ></FechaHoraLugar>
+    </div>
+
+    <div className= "check_form">
+      <p>Datos</p>
+      <Input name="Nombre" />
+      <Input name="Apellido" />
+      <Input name="Email" />
+    </div>
+
+    <Link to={`/`} className={'btn'}>Reservar</Link>
+ 
   </div>
-  <FechaHoraLugar ></FechaHoraLugar>
-  </>
 }
 
 export default Checkout;
