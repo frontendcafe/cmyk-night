aa136ee90a456209b592d465a2eda7cd4a893963
Florencia Licarzi <37675631+florencialicarzi@users.noreply.github.com>
2021-05-07 16:05:28 -0300

    add query params

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
 import FechaHoraLugar from '../components/FechaHoraLugar';
 import Avatar from '../components/Avatar';
-import "./style/Checkout.css"
 import Input from "../components/Input";
 import Dropdown from "../components/Dropdown";
 
+import "./style/Checkout.css"
 
-const Checkout = ({src, eventName, location, day, numberDay,hour}) => {
+
+const Checkout = () => {
+
+  const {search} = useLocation()
+  const searchParams = new URLSearchParams(search)
+  const src = searchParams.get('src')
+  const name = searchParams.get('name')
+  const location = searchParams.get('location')
+  const day = searchParams.get('day')
+  const numberDay = searchParams.get('numberDay')
+  const hour = searchParams.get('hour')
 
   return <div className="check_container">
 
     <div className="check_header">
       <div className="left_side">
-        <Avatar src={src} name={eventName}></Avatar>
+        <Avatar src={src} name={name}></Avatar>
         <Dropdown></Dropdown>
       </div>
       <div className="right_side">
